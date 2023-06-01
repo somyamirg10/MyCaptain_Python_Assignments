dd1df8cae951dbc765670d98e1268227a434487a
somyamirg10 <122742084+somyamirg10@users.noreply.github.com>
2023-06-01 20:48:55 +0530

    Print Positive numbers

---
+++ Positive Numbers.py
@@ -0,0 +1,15 @@
+# Python program to print positive Numbers in a List
+# list of numbers
+list1 = [12, -7, 5, 64, -14]
+for i in list1:
+    if i>=0:
+        print(i,' ' ,end='')
+
+
+# list of numbers
+list2 = [12, 14, -95, 3] 
+for i in list1:
+    if i>=0:
+        print(i,' ' ,end='')
+
+
